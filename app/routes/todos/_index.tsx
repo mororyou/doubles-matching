f8981de439e53275344a4857c184e8bf284ca799
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-10-02 21:28:26 +0900

    :clown_face: Hono api init

---
+++ app/routes/todos/_index.tsx
@@ -0,0 +1,25 @@
+
+import { useLoaderData } from "@remix-run/react"
+import { hc } from "hono/client"
+import { AppType } from "server"
+
+const client = hc<AppType>(import.meta.env.VITE_API_URL)
+
+export const loader = async () => {
+  const res = await client.api.todos.$get()
+  return res.json()
+}
+
+export default function Todos() {
+  const todos = useLoaderData<typeof loader>();
+  return (
+    <div className="flex flex-col">
+    Todos 
+      {todos.map((todo) => (
+        <div key={todo.id} className="flex items-center">
+          <span>{todo.text}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { dummyTodos } from "./demo/todos";
+
+const app = new Hono()
+
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+  })
+);
+
+const routes = app
+  .get("/api/todos", (c) => {
+    return c.json(dummyTodos);
+  });
+
+export default app
+  
+export type AppType = typeof routes;
