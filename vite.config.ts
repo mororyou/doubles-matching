c9af7252cdfeac0a487906e78a35370e2f6a7a6a
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-10-02 21:25:06 +0900

    :wrench: Vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
+import { flatRoutes } from 'remix-flat-routes';
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import adapter from '@hono/vite-dev-server/cloudflare'
+import serverAdapter from 'hono-remix-adapter/vite'
 
 export default defineConfig({
   plugins: [
@@ -14,6 +17,22 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__*.*',
+            '**/*.server.*',
+            '**/*.client.*',
+          ],
+        });
+      },
+    }),
+    serverAdapter({
+      adapter,
+      entry: 'server/index.ts',
     }),
     tsconfigPaths(),
   ],
